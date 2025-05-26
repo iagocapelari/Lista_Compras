ca9c939fe0235ae52d9ec362a6fb6bcae6159bfb
iagocapelari <160757982+iagocapelari@users.noreply.github.com>
2025-05-26 11:35:03 -0300

    aaaassss

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,7 +3,7 @@ import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 
 // Cria o client com a URL e a chave fornecidas
 export const supabase = createClient(
-  'https://rhtcddvlnecbamypmfep.supabase.co',
+  'https://ftsvfhukpviilrwwaiab.supabase.co',
  
-   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJodGNkZHZsbmVjYmFteXBtZmVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU2NDUsImV4cCI6MjA2MjExMTY0NX0.jlyg9dx-zcdiCXOrIJbm1UwbxNR36eV9dKZkiEsb9fQ'
+   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0c3ZmaHVrcHZpaWxyd3dhaWFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY1MzU2NjAsImV4cCI6MjA2MjExMTY2MH0.I8STwlkSYfLLPQYGHNn2G0NUO4Expe9pBRuBmbka9vM'
 )
